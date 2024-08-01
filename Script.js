886f61ae8aabe791a0f5a5e2ef29e3557c89ca1e
Harshit-7735 <letcs202205@global.org.in>
2024-08-01 13:15:25 +0530

    Javascript

--- Script.js
+++ Script.js
@@ -1,36 +1,86 @@
-let input = document.getElementById("input-box");
-let buttons = document.querySelectorAll("button");
-// First initialize the string to store the input
-let string = "";
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+const playerScoreDisplay = document.querySelector("#player-score");
+const computerScoreDisplay = document.querySelector("#computer-score");
+const messageDisplay = document.querySelector("#message h3");
 
-// Now we will loop through all the buttons and add an event listener to each of them
-let arr = Array.from(buttons);
-arr.forEach((button) => {
- switch (button.innerHTML) {
-    case "AC":
-     button.addEventListener("click", () => {
-        string = "";
-        input.value = string;
-     });
-     break;
-     case "DEL":
-        button.addEventListener("click", () => {
-            string = string.slice(0, -1);
-            input.value = string;
-        });
-        break;
-    case "=":
-     button.addEventListener("click", () => {
-        input.value = eval(string);
-        string = input.value;
-     });
-     break;
+const options = ["rock", "paper", "scissors"];
+let playerScore = 0,
+  computerScore = 0;
+
+const randomChoice = () => options[Math.floor(Math.random() * options.length)];
+
+const playRound = (playerSelection, computerSelection) => {
+    // for round 5
+    if (playerScore === 5) {
+        messageDisplay.innerHTML = "You win the game!"+ " " + "Congratulations! 🎉";
+        playerScore = 0;
+        computerScore = 0;
+        playerScoreDisplay.innerHTML = playerScore;
+        computerScoreDisplay.innerHTML = computerScore;
+        return;
+    } else if (computerScore === 5) {
+        messageDisplay.innerHTML = "You lose the game!"+ " " + "Better luck next time! 😢";
+        playerScore = 0;
+        computerScore = 0;
+        playerScoreDisplay.innerHTML = playerScore;
+        computerScoreDisplay.innerHTML = computerScore;
+        return;
+    }
+
+  switch (playerSelection) {
+    case "rock":
+      switch (computerSelection) {
+        case "rock":
+          messageDisplay.innerHTML = "Draw! &#129396";
+          break;
+        case "paper":
+          messageDisplay.innerHTML = "You lose! Paper beats rock. &#128534";
+          computerScoreDisplay.innerHTML = ++computerScore;
+          break;
+        case "scissors":
+          messageDisplay.innerHTML = "You win! Rock beats scissors. &#129395";
+          playerScoreDisplay.innerHTML = ++playerScore;
+          break;
+      }
+      break;
+    case "paper":
+      switch (computerSelection) {
+        case "rock":
+          messageDisplay.innerHTML = "You win! Paper beats rock. &#129395";
+          playerScoreDisplay.innerHTML = ++playerScore;
+          break;
+        case "paper":
+          messageDisplay.innerHTML = "Draw! &#129396";
+          break;
+        case "scissors":
+          messageDisplay.innerHTML = "You lose! Scissors beats paper. &#128534";
+          computerScoreDisplay.innerHTML = ++computerScore;
+          break;
+      }
+      break;
+    case "scissors":
+      switch (computerSelection) {
+        case "rock":
+          messageDisplay.innerHTML = "You lose! Rock beats scissors. &#128534";
+          computerScoreDisplay.innerHTML = ++computerScore;
+          break;
+        case "paper":
+          messageDisplay.innerHTML = "You win! Scissors beats paper. &#129395";
+          playerScoreDisplay.innerHTML = ++playerScore;
+          break;
+        case "scissors":
+          messageDisplay.innerHTML = "Draw! &#129396";
+          break;
+      }
+      break;
     default:
-     button.addEventListener("click", () => {
-        // Append the button value to the string
-        string += button.innerHTML;
-        input.value = string;
-     });
-     break;
-     }
-})
+      messageDisplay.innerHTML = "Invalid input! Please try again.";
+      break;
+  }
+};
+
+rock.addEventListener("click", () => playRound("rock", randomChoice()));
+paper.addEventListener("click", () => playRound("paper", randomChoice()));
+scissors.addEventListener("click", () => playRound("scissors", randomChoice()));
